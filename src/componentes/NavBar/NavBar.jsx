9593bf465b529500c3e64b648f84a02b4a349787
elton971 <82831720+elton971@users.noreply.github.com>
2022-05-05 22:38:19 +0200

    update navbar

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export const NavBar = () => {
     var classe="nav-links";
     const [isOpen, setIsOpen] = useState(true);
-    const toggle = () => setIsOpen(!isOpen);
+    const toggle = () => setIsOpen(!isOpen);//seta o estado de aberto ou fechado da navbar
     return (
         <header className="header">
             <nav className="nav__bar">
@@ -21,10 +21,10 @@ export const NavBar = () => {
                     }
                 }}>&equiv;</label>
                 <ul className={isOpen ? "nav-links show" : "nav-links"}>
-                    <li className="nav-links__item" onClick={toggle}><a href="#home">Home</a></li>
-                    <li className="nav-links__item"><a href="#sobre">Sobre</a></li>
-                    <li className="nav-links__item"><a href="#skills">Skills</a></li>
-                    <li className="nav-links__item"><a href="#contacto">Contacto</a></li>
+                    <li className="nav-links__item" onClick={toggle} ><a href="#home">Home</a></li>
+                    <li className="nav-links__item" onClick={toggle} ><a href="#sobre">Sobre</a></li>
+                    <li className="nav-links__item" onClick={toggle} ><a href="#skills">Skills</a></li>
+                    <li className="nav-links__item" onClick={toggle} ><a href="#contacto">Contacto</a></li>
                 </ul>
             </nav>
         </header>
